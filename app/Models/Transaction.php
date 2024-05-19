e7bd936b5297ee3f88485b47bb504b1f4aa47871
Arman Nugraha <armnugraha@envisions.app>
2024-05-19 16:20:39 +0700

    add: migration & model

---
+++ app/Models/Transaction.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'trx_code',
+        'total_price',
+        'total_payment',
+        'short_url',
+        'full_url'
+    ];
+}
