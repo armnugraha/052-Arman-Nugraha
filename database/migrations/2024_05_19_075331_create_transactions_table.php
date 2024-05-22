36f55468e72b065b11c980a42fadb3ecfc27df00
arm_nugraha <armnugraha@envisions.app>
2024-05-22 17:01:57 +0700

    ref: migration transaction

--- database/migrations/2024_05_19_075331_create_transactions_table.php
+++ database/migrations/2024_05_19_075331_create_transactions_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
             $table->string('trx_code')->unique();
             $table->integer('total_price');
             $table->integer('total_payment');
-            $table->string('short_url')->unique();
-            $table->string('full_url')->unique();
+            $table->string('short_url')->nullable()->unique();
+            $table->string('full_url')->nullable()->unique();
             $table->timestamps();
         });
     }
