c9c5fa7b6f92ebb4c2fcf1f887f965c3dea7bf24
Arman Nugraha <armnugraha@envisions.app>
2024-05-24 16:07:47 +0700

    change migration

--- database/migrations/2024_05_19_075331_create_transactions_table.php
+++ database/migrations/2024_05_19_075331_create_transactions_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->index();
+            $table->string('name')->nullable();
             $table->string('trx_code')->unique();
             $table->integer('total_price');
             $table->integer('total_payment');
